89f16a3a95a39fe3ad7e1cc398ea6398c0257f35
Donato <jigsaw-92@live.it>
2021-06-18 12:53:14 +0200

    Added promise event socket io managing to block double clicks on emits

--- src/geodata/geodata.gateway.ts
+++ src/geodata/geodata.gateway.ts
@@ -14,6 +14,7 @@ import { trucks } from '../data/trucks';
 import { geoDataTrucks } from '../data/tracks';
 import { TruckSearchDto } from './dto/request/truck-search.dto';
 import { TruckDto } from './dto/track.dto';
+import * as util from 'util';
 
 interface connectedClient {
   connected: boolean;
@@ -26,14 +27,14 @@ export class GeoDataGateway
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('ChatGateway');
   private clients = new Map<string, connectedClient>();
-  private clientEmits = new Map<string, NodeJS.Timeout[]>();
+  private clientEmits = new Map<string, Promise<void>>();
 
   constructor(private readonly getDataService: GeoDataService) {}
 
   public handleConnection(client: Socket): void {
     client.emit('connected');
     this.clients.set(client.id, { connected: true });
-    this.clientEmits.set(client.id, []);
+    this.clientEmits.set(client.id, null);
     this.logger.log(`Client connected: ${client.id}`);
     this.logger.log(`#clients ${this.clients.size}`);
   }
@@ -54,21 +55,19 @@ export class GeoDataGateway
 
   @SubscribeMessage('trackTravel')
   async trackTravel(client: Socket, payload: TruckSearchDto): Promise<void> {
-    this.logger.log(`Trying to retrive tracks for ${payload.truck}`);
+    const pendingPromiseEmit = this.clientEmits.get(client.id);
 
-    const tracks = geoDataTrucks.find(
-      (data) => data.id === payload.truck,
-    ).track;
+    if (!util.inspect(pendingPromiseEmit).includes('pending')) {
+      this.logger.log(`Trying to retrive tracks for ${payload.truck}`);
 
-    this.clientEmits.get(client.id).forEach((timeout) => {
-      clearTimeout(timeout);
-    });
+      const tracks = geoDataTrucks.find(
+        (data) => data.id === payload.truck,
+      ).track;
 
-    this.clientEmits.set(client.id, []);
-
-    tracks.map((track, index) => {
-      this.emitWithDelay(client, index, track, payload.truck);
-    });
+      tracks.map((track, index) => {
+        this.emitWithDelay(client, index, track, payload.truck);
+      });
+    }
   }
 
   public afterInit(server: Server): void {
@@ -82,10 +81,13 @@ export class GeoDataGateway
     truck: string,
   ): void {
     const logger = this.logger;
-    const timeout = setTimeout(function () {
-      logger.log(`Sending track ${JSON.stringify(track)} for truck ${truck}`);
-      client.emit('tracks', track);
-    }, index * 3000);
-    this.clientEmits.get(client.id).push(timeout);
+    const emitPromise = new Promise<void>((resolve) => {
+      setTimeout(function () {
+        logger.log(`Sending track ${JSON.stringify(track)} for truck ${truck}`);
+        client.emit('tracks', track);
+        resolve();
+      }, index * 3000);
+    });
+    this.clientEmits.set(client.id, emitPromise);
   }
 }
